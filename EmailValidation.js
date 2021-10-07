ec595100ceb26a3709461166428d452d83bdf845
padmini v <padmini.v@perfios.com>
2021-10-07 23:42:22 +0530

    REFACTORED: regex to validate email with optional country code

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,19 +1,38 @@
-const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9.]*[_ + - .]?[a-zA-Z0-9]*[@]{1}[a-zA-Z0-9]*[.]{1}[a-zA-Z]*$");  
-
+const emailRegex = RegExp("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z]+[.]?[a-zA-Z]+$");
 function validateEmail(email)
 {
     if(emailRegex.test(email))
         console.log(email + " is Valid");
     else
-        throw email + " is Invalid";
+        console.log(email + " is Invalid");
 }
 try
 {
-    validateEmail("abc.de+abc@bridgelabz.com");
-    validateEmail("abc.abc@bridgelabz.com");
-    validateEmail("abc@bridgelabz.com");
-    validateEmail("abc+abc@bridgelabz.com");
-    validateEmail(".abc@abc.com");
+validateEmail("abc@bridgelabz.co.in");
+validateEmail( "abc@gmail.com.com");
+validateEmail( "abc@yahoo.com");
+validateEmail( "abc@1.com");
+validateEmail( "abc-100@yahoo.com");
+validateEmail( "abc.100@yahoo.com");
+validateEmail( "abc111@abc.com");
+validateEmail( "abc-100@abc.net");
+validateEmail( "abc.100@abc.com.au");
+validateEmail( "abc+100@gmail.com");
+
+console.log("---------- Invalid Emails ----------");
+
+validateEmail( ".abc@abc.com");
+validateEmail( "abc");
+validateEmail( "abc@.com.my");
+validateEmail( "abc@abc@gmail.com");
+validateEmail( "abc()*@gmail.com");
+validateEmail( "abc..2002@gmail.com");
+validateEmail( "abc.@gmail.com");
+validateEmail( "abc123@.com");
+validateEmail( "abc123@.com.com");
+validateEmail( "abc123@gmail.a");
+validateEmail( "abc@%*.com");
+validateEmail( "abc@gmail.com.aa.au");
 }
 catch(e)
 {
